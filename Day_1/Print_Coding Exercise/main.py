4f770c578e2594e05be94da86df0010fdb5a3fb1
Brian <wamwangimathenge@gmail.com>
2023-10-30 10:05:29 +0300

    Concatenate two strings

--- Day_1/Print_Coding Exercise/main.py
+++
@@ -1,1 +0,0 @@
-print("Day 1 - Python Print Function \nThe function is declared like this: \nprint('what to print)")
---
+++ Day_1/string_manipulation.py
@@ -0,0 +1,5 @@
+# Printing multiple lines using print function using "\n"
+print("Hello Brian\nHello Brian\nHello Brian")
+
+# String concatenation
+print(f"My name is Brian")
