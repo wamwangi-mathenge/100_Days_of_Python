0efcc742a9f5f30201c080e05c5e68d0c5e3631a
Brian <wamwangimathenge@gmail.com>
2023-10-30 11:02:24 +0300

    Switch the values stored in the variables

---
+++ Day_1/Variables_Exercise/main.py
@@ -0,0 +1,18 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+
+# Create a temporary variable 'temp' and assign it to a
+temp = a
+# Assign a to the value of b
+a = b
+# Assign b to the temp value
+b = temp
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
