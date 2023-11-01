1a84b0390fb7045a7fd18203fe581c0a9a9b7c3f
Brian <wamwangimathenge@gmail.com>
2023-11-01 08:24:25 +0300

    Perform basic math operations in python

---
+++ Day_2/math_operations.py
@@ -0,0 +1,25 @@
+# Addition
+print(3 + 5)
+
+# Subtraction
+print(7 - 4)
+
+# Multiplication
+print(3 * 2)
+
+# Division
+print(6 / 3)
+
+# Exponential
+print(4 ** 2)
+
+# PEMDAS
+# 1. Parentheses ()
+# 2. Exponential **
+# 3. Multiplication *
+# 4. Division /
+# 5. Addition +
+# 6. Subtraction -
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
