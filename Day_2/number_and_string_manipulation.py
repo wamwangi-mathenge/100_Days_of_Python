49c6493cb19ceb2d8c20867627b169aec0f653b5
Brian <wamwangimathenge@gmail.com>
2023-11-01 08:59:27 +0300

    Perform basic number and string manipulation

---
+++ Day_2/number_and_string_manipulation.py
@@ -0,0 +1,13 @@
+# Rounding off to specified number of decimal places
+print(round(8 / 3, 2))
+
+# Floor division
+print(5 // 2)
+print(type(5 // 2))
+
+# F-strings
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"Your score is {score}, your height is {height}, and you are winning is {isWinning}")
