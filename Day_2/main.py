46462137cf9389d2c394c5ca42d2207678142302
Brian <wamwangimathenge@gmail.com>
2023-11-01 07:50:57 +0300

    Examine the different data types in python

---
+++ Day_2/main.py
@@ -0,0 +1,25 @@
+print(len("Hello")) # Returns the length of the string
+
+# print(len(12345)) # Returns an error
+
+
+# Data Types
+# String
+"Hello"
+# Subscripting
+print("Hello"[0]) # Returns the item at position 0
+print("Hello"[-1]) # Returns the item in the last position
+# Numbers enclosed in single or double quotes are treated as strings
+print(type("123"))
+print("123" + "456")
+
+# Integer
+print(type(123))
+print(123 + 456)
+
+# Float => Numbers with decimal places
+print(type(3.14159))
+
+# Boolean => True & False
+print(type(True))
+print(type(False))
