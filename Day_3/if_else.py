d28152a2af970590d587733e832326b89a732fd6
Brian <wamwangimathenge@gmail.com>
2023-11-02 10:17:27 +0300

    Create a rollercoaster program
    The program uses height input to determine whether one is eligible to
    ride the rollercoaster or not.

---
+++ Day_3/if_else.py
@@ -0,0 +1,9 @@
+# Create a rollercoaster program to determine ride eligibility
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
