e47bd76de0b4052f7661947a7537307abb6fd15f
Brian <wamwangimathenge@gmail.com>
2023-11-02 14:48:20 +0300

    Add a want_photo variable
    This commit adds a 'want_photo' variable that adds prices if the
    customer wants their photo taken in the rollercoaster

--- Day_3/nested_if_else.py
+++ Day_3/nested_if_else.py
@@ -2,16 +2,26 @@
 
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("How old are you? "))
     
     if age > 18:
-        print("Pay $12")
+        bill = 12
+        print("Adult Prices are $12")
     elif age >= 12:
-        print("Pay $7")
+        bill = 7
+        print("Youth Prices are $7")
     else:
-        print("Pay $5")
+        bill = 5
+        print("Child Prices are $5")
+        
+    want_photo = input("Do you want your photo taken? Y or N ")
+    if want_photo == "Y":
+        bill+=3
+    
+    print(f"Your total bill is {bill}")
 else:
     print("Sorry, you have to grow taller before you can ride")
