fcaf38520ad33853f6e17ebe1fe3da599688a39a
Brian <wamwangimathenge@gmail.com>
2023-11-03 09:21:08 +0300

    Perform basic list operations

---
+++ Day_4/lists.py
@@ -0,0 +1,20 @@
+states_of_america = [
+    "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut",
+    "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia",
+    "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky",
+    "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi",
+    "Illinois", "Alabama", "Maine", "Missouri", "Arkansas",
+    "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
+    "California", "Minnesota", "Oregon", "Kansas", "West Virginia",
+    "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota",
+    "Montana", "Washington", "Idaho", "Wyoming", "Utah",
+    "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"
+]
+
+
+print(states_of_america[0]) # State that joined the union first
+print(states_of_america[20])
+print(states_of_america[48])
+
+states_of_america.append("Mexico")
+print(states_of_america)
