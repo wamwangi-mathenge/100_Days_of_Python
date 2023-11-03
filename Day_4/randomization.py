a699092e2fa70aef9d86b2750ccb43da0f8d9fbd
Brian <wamwangimathenge@gmail.com>
2023-11-03 08:50:45 +0300

    Create and use a custom module

--- Day_4/randomization.py
+++ Day_4/randomization.py
@@ -1,6 +1,9 @@
 # Import the random module.
 import random
+import my_module
 
-random_integer = random.randint(1, 10) # (1, 10) represents the range
+# random_integer = random.randint(1, 10) # (1, 10) represents the range
 
-print(random_integer)
+# print(random_integer)
+
+print(my_module.pi)
