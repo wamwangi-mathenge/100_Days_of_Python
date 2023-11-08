54d72491a0b4ff633638ad1de37aafe1cb3b323b
Brian <wamwangimathenge@gmail.com>
2023-11-08 10:56:04 +0300

    Use the range function in a for loop

--- Day_5/Loops/main.py
+++ Day_5/Loops/main.py
@@ -6,3 +6,24 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     # print(fruit)
     print(f"{fruit} pie")
+    
+print("-----------------------")
+    
+# Range Function
+for number in range(1, 11):
+    print(number)
+    
+print("--------------------------")
+    
+# To change the increment value
+for number2 in range(1, 20, 3):
+    print(number2)
+    
+print("------------------------")
+
+# Increment the values from 1 to 100
+total = 0
+for number3 in range(1, 101):
+    total+=number3
+    
+print(total)
