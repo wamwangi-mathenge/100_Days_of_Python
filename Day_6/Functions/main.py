94f5ef656fd15fbb6252b3e10b5bbcdc0dcac918
Brian <wamwangimathenge@gmail.com>
2023-11-09 10:26:27 +0300

    Define and call a function

---
+++ Day_6/Functions/main.py
@@ -0,0 +1,17 @@
+# Functions
+
+# Built-in Functions
+print("Hello Brian!")
+
+numchar = len("Hello")
+print(numchar)
+
+
+
+# Defining a Function
+def my_function():
+    print("Hello")
+    print("Goodbye")
+
+# Calling a Function
+my_function()
