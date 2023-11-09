0595f66afca411d31ab80154d6b039e5c269b62d
Brian <wamwangimathenge@gmail.com>
2023-11-09 12:43:57 +0300

    Describe while loops

---
+++ Day_6/While_loop/main.py
@@ -0,0 +1,16 @@
+# While Loop Syntax
+
+# while something_is_true:
+    # Do this
+    # Then do this
+    # Then do this
+    
+    
+# For loops are great when you need to iterate over something and you need to do something with each thing you're iterating over.
+
+fruits = ["Apple", "Pear", "Orange"]
+for fruit in fruits:
+    print(fruit)
+    
+# While loops are used when you don't care what number in the sequence you're in, which item you're iterating over in the list
+# Used when you want to carry out some functionality, many times until some condition is met
