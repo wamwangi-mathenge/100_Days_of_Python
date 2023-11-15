ab4fe923fdf33a1a76ed2bf7a92ab5db69016770
Brian <wamwangimathenge@gmail.com>
2023-11-15 11:11:44 +0300

    Reposition print statement

--- Day_7/Hangman/Hangman2.py
+++ Day_7/Hangman/Hangman2.py
@@ -31,7 +31,8 @@ for idx, letter in enumerate(chosen_word):
     if letter == guess:
         display[idx] = letter
     
-print(display)
+
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+print(display)
