699d0b7d641152391a4b94c81e137a7aa2e3007d
Brian <wamwangimathenge@gmail.com>
2023-11-15 11:09:25 +0300

    Reveal that letter in the display at that position
    This commit loops through each position in the chosen_word and if the
    letter at that position matches 'guess', it reveals that letter in the
    display at that position.

--- Day_7/Hangman/Hangman2.py
+++ Day_7/Hangman/Hangman2.py
@@ -21,11 +21,17 @@ guess = input("Guess a letter: ").lower()
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for letter in chosen_word:
+# for letter in chosen_word:
+#     if letter == guess:
+#         print("Right")
+#     else:
+#         print("Wrong")
+        
+for idx, letter in enumerate(chosen_word):
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[idx] = letter
+    
+print(display)
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
