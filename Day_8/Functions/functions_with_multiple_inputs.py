2f0be646248eb0a0845c089110ed6f6ae4f20a6a
Brian <wamwangimathenge@gmail.com>
2023-11-16 09:35:58 +0300

    Call a function using positional arguments

---
+++ Day_8/Functions/functions_with_multiple_inputs.py
@@ -0,0 +1,9 @@
+# Functions with Multiple Inputs
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"Welcome to {location}")
+    
+# Calling the function using a positional argument
+greet_with("Brian", "London")
+greet_with("Maxine", "Paris")
