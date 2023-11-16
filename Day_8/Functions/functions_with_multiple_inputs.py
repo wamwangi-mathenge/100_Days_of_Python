51b12ad68465b98c342c8499f59cf5f001d9c609
Brian <wamwangimathenge@gmail.com>
2023-11-16 09:38:52 +0300

    Call a function using keyword arguments

--- Day_8/Functions/functions_with_multiple_inputs.py
+++ Day_8/Functions/functions_with_multiple_inputs.py
@@ -4,6 +4,12 @@ def greet_with(name, location):
     print(f"Hello {name}")
     print(f"Welcome to {location}")
     
-# Calling the function using a positional argument
+# Calling the function using Positional Arguments
 greet_with("Brian", "London")
 greet_with("Maxine", "Paris")
+
+print("-------------------------------------")
+
+# Calling the function using Keyword Arguments
+greet_with(name="Joe", location="Washington")
+greet_with(name="Vladimir", location="Moscow")
