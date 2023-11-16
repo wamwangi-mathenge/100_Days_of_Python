bd5626a8431ef0e4d0351a78c1adff9fa5b05f23
Brian <wamwangimathenge@gmail.com>
2023-11-16 09:20:23 +0300

    Review function concepts
    This commit creates a function called greet().
    The function has 3 print statements inside it.
    Finally the function is called.

---
+++ Day_8/Functions/function.py
@@ -0,0 +1,14 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+name = input("What is your name? ")
+
+def greet():
+    print(f"Hello {name}.")
+    print(f"Welcome to 100 Days of Python😁")
+    print(f"Let's get it💪🏽!!!")
+    
+    
+greet()
