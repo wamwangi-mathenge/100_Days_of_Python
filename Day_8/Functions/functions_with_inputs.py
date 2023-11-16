ce88253e8dcf180059f119484acf28a18b5192ef
Brian <wamwangimathenge@gmail.com>
2023-11-16 09:28:59 +0300

    Create and call a function with inputs

---
+++ Day_8/Functions/functions_with_inputs.py
@@ -0,0 +1,8 @@
+# Functions that allow for inputs
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+    
+greet_with_name("Brian")
+greet_with_name("Maxine")
