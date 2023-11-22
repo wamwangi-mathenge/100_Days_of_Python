d3b798e4fd6a97d97b17ed8af21f79b44db634cc
Brian <wamwangimathenge@gmail.com>
2023-11-22 12:53:46 +0300

    Cater for multiple bids to be made

--- Day_9/Secret_Auction/main.py
+++ Day_9/Secret_Auction/main.py
@@ -4,15 +4,19 @@ print(logo)
 print("Welcome to the Secret Auction Program")
 
 bidders = {}
-
-name = input("What is your name? ")
-bid = input("What is your bid? $")
-
-# bidders[name] = bid
-# print(bidders)
-
 def add_bid(name, bid):
     bidders[name] = bid
     print(bidders)
+
+continue_bid = True
+while continue_bid:
+
+    name = input("What is your name? ")
+    bid = input("What is your bid? $")
+
+    add_bid(name, bid)
     
-add_bid(name, bid)
+    choice = input(("Are there any other bidders? Type 'yes' or 'no'. "))
+    if choice == "no":
+        continue_bid = False
+        print("Bids Closed")
