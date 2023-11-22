d8434a9029de4042ac58d45ef4ab421aba041e2e
Brian <wamwangimathenge@gmail.com>
2023-11-22 13:55:58 +0300

    Find the highest bidder

--- Day_9/Secret_Auction/main.py
+++ Day_9/Secret_Auction/main.py
@@ -2,23 +2,31 @@ from logo import logo
 
 print(logo)
 print("Welcome to the Secret Auction Program")
-
-bidders = {}
-# max_value = 0
-# max_key = None
- 
-        
-
 continue_bid = True
+bidders = {}
+
+def find_highest_bid(bidders):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidders:
+        bid_amount = bidders[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
 while continue_bid:
 
     name = input("What is your name? ")
-    bid = input("What is your bid? $")
+    bid = int(input("What is your bid? $"))
 
     bidders[name] = bid
-    print(bidders)
+    # print(bidders)
+    # find_highest_bid(bidders)
     
     choice = input(("Are there any other bidders? Type 'yes' or 'no'. "))
     if choice == "no":
         continue_bid = False
-        print("Bids Closed")
+        print("Bids Closed!!")
+        find_highest_bid(bidders)
+
