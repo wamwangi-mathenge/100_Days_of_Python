9ddecd927788f243c428ecf6bd0519ec72c9a2c4
Brian <wamwangimathenge@gmail.com>
2023-11-22 12:43:37 +0300

    Create an add to bid function

--- Day_9/Secret_Auction/main.py
+++ Day_9/Secret_Auction/main.py
@@ -8,3 +8,11 @@ bidders = {}
 name = input("What is your name? ")
 bid = input("What is your bid? $")
 
+# bidders[name] = bid
+# print(bidders)
+
+def add_bid(name, bid):
+    bidders[name] = bid
+    print(bidders)
+    
+add_bid(name, bid)
