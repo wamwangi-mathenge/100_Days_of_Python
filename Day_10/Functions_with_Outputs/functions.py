cde3b021faf34edf05991d752744bf80d4447586
Brian <wamwangimathenge@gmail.com>
2023-11-23 10:33:13 +0300

    Create a function with multiple return statements

--- Day_10/Functions_with_Outputs/functions.py
+++ Day_10/Functions_with_Outputs/functions.py
@@ -1,9 +1,20 @@
 # Functions with outputs
 
-def format_name(f_name, l_name):
+# def format_name(f_name, l_name):
+#     formatted_f_name = f_name.title()
+#     formatted_l_name = l_name.title()
+    
+#     return f"{formatted_f_name} {formatted_l_name}"
+    
+# print(format_name("brian", "MATHENGE"))
+
+# Function with multiple return statements
+def format_name2(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs"
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
     
     return f"{formatted_f_name} {formatted_l_name}"
-    
-print(format_name("brian", "MATHENGE"))
+
+print(format_name2(input("What is your first name? "), input("What is your last name? ")))
