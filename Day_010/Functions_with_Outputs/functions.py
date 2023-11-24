80830f2ea6cea3b0e84230fbe2327c8be216da4d
Brian <wamwangimathenge@gmail.com>
2023-11-24 09:38:48 +0300

    Add docstrings to function

--- Day_010/Functions_with_Outputs/functions.py
+++ Day_010/Functions_with_Outputs/functions.py
@@ -10,6 +10,10 @@
 
 # Function with multiple return statements
 def format_name2(f_name, l_name):
+    """
+    Take a first and last name and format it to return the 
+    title case version of the name
+    """
     if f_name == "" or l_name == "":
         return "You didn't provide valid inputs"
     formatted_f_name = f_name.title()
