a996aff6521c0860822db821e3fc9e461dfb82dc
Brian <wamwangimathenge@gmail.com>
2023-11-24 10:16:16 +0300

    Reuse the 1st answer as a parameter for the 2nd

--- Day_010/Calculator/calculator.py
+++ Day_010/Calculator/calculator.py
@@ -39,7 +39,14 @@ operation_symbol = input("Pick an operation from the line above: ")
 num2 = int(input("What's the second number? "))
 
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+first_answer = calculation_function(num1, num2)
 
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("Pick another operation: ")
+num3 = int(input("What's the next number?: "))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(first_answer, num3)
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
