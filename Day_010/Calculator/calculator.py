f5740ca5195623a4ada2c845b565a1f72596fc94
Brian <wamwangimathenge@gmail.com>
2023-11-24 09:50:33 +0300

    Create a dictionary that stores the operations

--- Day_010/Calculator/calculator.py
+++ Day_010/Calculator/calculator.py
@@ -15,3 +15,13 @@ def multiply(n1, n2):
 # Divide
 def divide(n1, n2):
     return n1 / n2
+
+
+# Create a dictionary where the keys are each of these symbols and the values are the names of the functions.
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
