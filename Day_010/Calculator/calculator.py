371f29f945f8a11307aa6f7b991077cb46704b28
Brian <wamwangimathenge@gmail.com>
2023-11-24 10:48:06 +0300

    Enable for continuous calculations

--- Day_010/Calculator/calculator.py
+++ Day_010/Calculator/calculator.py
@@ -26,27 +26,36 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("What's the first number?: "))
+# Use a while loop to allow the user to conduct many calculations
+
+def calculator():
+    num1 = int(input("What's the first number?: "))
 
 
-# Use a for loop to loop through "operations" and print out each of the symbols
+    # Use a for loop to loop through "operations" and print out each of the symbols
 
-for key in operations:
-    print(key)
-    
-operation_symbol = input("Pick an operation from the line above: ")
+    for key in operations:
+        print(key)
+        
+    continue_calculation = True
 
-num2 = int(input("What's the second number? "))
+    while continue_calculation:
+        operation_symbol = input("Pick an operation: ")
 
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1, num2)
+        num2 = int(input("What's the next number? "))
 
-
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(first_answer, num3)
-
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: " )
+        if choice == 'y':
+            num1 = answer
+        else:
+            continue_calculation = False
+            # Recursion => Call the very first function and start a new calculation
+            # WARNING: Make sure that there is a condition before a function can call itself to avoid an infinite loop
+            calculator()
+            
+            
+calculator()
